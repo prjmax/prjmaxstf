e51bd44c124913b93d0d7af1c01da8c3accd5d40
prjmax <prjmax@gmail.com>
2016-07-05 18:45:35 +1200

    initial commit

---
+++ safe to fly/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  safe to fly
+//
+//  Created by pablo rendon on 5/07/16.
+//  Copyright © 2016 prjmax. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
